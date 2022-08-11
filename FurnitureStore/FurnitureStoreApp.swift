300317156347a8681b50cf28e0ccd3053a61cd86
Darya <lilleeash@icloud.com>
2022-08-11 12:18:24 +0300

    Initial Commit

---
+++ FurnitureStore/FurnitureStoreApp.swift
@@ -0,0 +1,17 @@
+//
+//  FurnitureStoreApp.swift
+//  FurnitureStore
+//
+//  Created by Darya Zhitova on 11.08.2022.
+//
+
+import SwiftUI
+
+@main
+struct FurnitureStoreApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
